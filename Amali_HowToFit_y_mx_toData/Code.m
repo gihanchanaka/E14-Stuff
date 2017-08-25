cc5e0ca506383daa2569943d78db973c228f8467
gihanchanaka <gihanchanaka@gmail.com>
2017-08-25 19:48:31 +0530

    Adding the curve fit code

---
+++ Amali_HowToFit_y_mx_toData/Code.m
@@ -0,0 +1,28 @@
+X=[32.8,34.4,34.4,33.5,32.9,29.3];
+Y=[17.83,16.38,15.68,14.66,14.16,12.83];
+
+mMin=min(Y./X);
+mMax=max(Y./X);
+
+mBest=0;
+leastError=inf;
+
+for m = linspace(mMin,mMax,200)
+    error=sum((Y-X*m).^2);
+    if error < leastError
+        leastError=error;
+        mBest=m;
+    end
+end
+
+mBest
+leastError
+
+plot(linspace(0,max(X)),linspace(0,max(X))*mBest,'b- ');
+hold on;
+plot(X,Y,'r *');
+
+figure
+plot(linspace(min(X),max(X)),linspace(min(X),max(X))*mBest,'b- ');
+hold on;
+plot(X,Y,'r *');
