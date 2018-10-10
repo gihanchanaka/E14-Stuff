f3fa4efdc296b95a5973927f48936dfef7819289
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2018-10-10 17:49:24 +0530

    Update MainProg.py

--- Hasitha_IESL/MainProg.py
+++ Hasitha_IESL/MainProg.py
@@ -2,6 +2,14 @@ import cv2
 import numpy as np
 import RPi.GPIO as GPIO
 
+TURNING_PARAM=-0.1
+FORWARD_PARAM=20
+
+TOP_OFFSET=100
+FRAME_HEIGHT=600-100
+LEFT_OFFSET=250
+FRAME_WIDTH=900-250
+
 cap=cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
@@ -10,18 +18,17 @@ if __name__== "__main__":
     STATE="BEGIN"
     while True:
         if STATE=="BEGIN":
+            H=FRAME_HEIGHT
+            W=FRAME_WIDTH
             ret,frame=cap.read()
             while(ret==False):
                 ret,frame=cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             blur=cv2.GaussianBlur(gray,(5,5),0)
             ret,th1 = cv2.threshold(blur,35,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            fr=th1#[100:600,250:900]
+            fr=th1[TOP_OFFSET:H+TOP_OFFSET,LEFT_OFFSET:W+LEFT_OFFSET]
             cv2.imshow('otsu',th1)
             cv2.waitKey(10)
-
-            H=fr.shape[0]
-            W=fr.shape[1]
             COG=np.zeros(shape=(H),dtype=np.int)
             M=np.zeros(shape=(H),dtype=np.int)
             STATE="LINE-FOLLOW"
@@ -34,7 +41,7 @@ if __name__== "__main__":
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             blur=cv2.GaussianBlur(gray,(5,5),0)
             ret,th1 = cv2.threshold(blur,35,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            fr=th1#[100:600,250:900]
+            fr=th1[TOP_OFFSET:H+TOP_OFFSET,LEFT_OFFSET:W+LEFT_OFFSET]
             cogFrame=np.zeros(fr.shape)
 
 
@@ -61,19 +68,19 @@ if __name__== "__main__":
 
             COG2=COG-W/2
             np.flip(COG2,0)
-            dir=np.dot(COG2,np.exp(-0.005*np.arange(H,0,-1)))/np.sum(np.exp(-0.005*np.arange(H,0,-1)))
+            dir=np.dot(COG2,np.exp(TURNING_PARAM*np.arange(H,0,-1)))/np.sum(np.exp(TURNING_PARAM*np.arange(H,0,-1)))
 
             cogFrame[:,int(dir+W/2)]=1
 
-            thresNoTurn=20
 
-            if(np.abs(dir)<thresNoTurn):
-                print(dir,"Forward")
+
+            if(np.abs(dir)<FORWARD_PARAM):
+                print(dir,"FORWARD")
             else:
                 if(dir<0):
-                    print(dir,"CCW")
+                    print(dir,"RIGHT-Forward")
                 else:
-                    print(dir,"CW")
+                    print(dir,"LEFT-Forward")
 
             cv2.imshow('otsu',fr)
             cv2.waitKey(10)
